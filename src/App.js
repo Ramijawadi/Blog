a69b4db867976c39b4b2d1aa7dff534365f7d032
Jawadi Rami <67319552+Ramijawadi@users.noreply.github.com>
2024-06-29 15:44:32 +0100

    Create reusable component Articles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import ArticlesList from './pages/ArticlesList';
+import Article from './pages/Article';
+
+import { BrowserRouter as Router , Routes ,Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navbar />
+    <div className="max-w-screen-md mx-auto pt-20">
+  
+<Routes>
+<Route path='/'  element={<Home/>}/>
+<Route path='/about'  element={<About/>}/>
+<Route path='/article/:name'  element={<Article/>}/>
+<Route path='/articles-list' element={<ArticlesList/>}/>
+</Routes>
+</div>
+
+ </Router>
+   
   );
 }
 
 export default App;
+ 
---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function About() {
+  return (  
+  <div className='mb-20'>
+
+    <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>About</h1>
+
+    <p className='mx-auto leading-relaxed text-base mb-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+<p className='mx-auto leading-relaxed text-base mb-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+<p className='mx-auto leading-relaxed text-base mb-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+</div>
+  )
+}
+
+export default About
